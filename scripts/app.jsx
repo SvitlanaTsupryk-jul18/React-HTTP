ac295e87d21e730a074e94d43c1bf360b776beb3
Svet <stsupryk@gmail.com>
2019-09-01 22:06:32 +0300

    add succsess message

--- scripts/app.jsx
+++ scripts/app.jsx
@@ -13,52 +13,60 @@ class App extends Component {
     setCountries(countries);
   }
   render() {
-    const { handleChange, validationInput, warning } = this.props;
+    const { handleChange, validationInput, warning, sendForm, signed } = this.props;
     return (
-      <form className="form" onSubmit={postUser}>
-        <h2>Sigh up</h2>
-        <p className="description">Name</p>
-        <input
-          type="text"
-          name="userName"
-          onChange={(event) => handleChange(event.target)}
-          onBlur={(event) => validationInput(event.target)}
-          placeholder="Name"
-        // required 
-        />
-        <Number />
-        <input
-          type="email"
-          name="userEmail"
-          onChange={(event) => handleChange(event.target)}
-          onBlur={(event) => validationInput(event.target)}
-          placeholder="Email address"
-        // required 
-        />
-        <Country />
-        <p className="description">Password</p>
-        <input
-          type="password"
-          name="userPassword"
-          onChange={(event) => handleChange(event.target)}
-          onBlur={(event) => validationInput(event.target)}
-          placeholder=""
-        // required 
-        />
-        <p className="description">Password confirmation</p>
-        <input
-          type="password"
-          name="passwordConfirm"
-          onChange={(event) => handleChange(event.target)}
-          onBlur={(event) => validationInput(event.target)}
-          placeholder=""
-        // required 
-        />
-        <p className="warning">{warning}</p>
-        <input type="checkbox" name="subscribe" name="subscribe" />
-        <label htmlFor="subscribe">Horns</label>
-        <input type="submit" value="Отправить" />
-      </form>
+      signed ? (
+        <div className="message">
+          <h3>Great</h3>
+          <p>your account has been successfully created.</p>
+        </div>
+      ) : (
+          <form className="form" onSubmit={sendForm}>
+            <h2>Sigh up</h2>
+            <p className="description">Name</p>
+            <input
+              type="text"
+              name="userName"
+              onChange={(event) => handleChange(event.target)}
+              onBlur={(event) => validationInput(event.target)}
+              placeholder="Name"
+              required
+            />
+            <Number />
+            <input
+              type="email"
+              name="userEmail"
+              onChange={(event) => handleChange(event.target)}
+              onBlur={(event) => validationInput(event.target)}
+              placeholder="Email address"
+              required
+            />
+            <Country />
+            <p className="description">Password</p>
+            <input
+              type="password"
+              name="userPassword"
+              onChange={(event) => handleChange(event.target)}
+              onBlur={(event) => validationInput(event.target)}
+              placeholder=""
+              required
+            />
+            <p className="description">Password confirmation</p>
+            <input
+              type="password"
+              name="passwordConfirm"
+              onChange={(event) => handleChange(event.target)}
+              onBlur={(event) => validationInput(event.target)}
+              placeholder=""
+              required
+            />
+            <p className="warning">{warning}</p>
+            <p className="text"><input type="checkbox" name="subscribe" name="subscribe" />
+              <label htmlFor="subscribe">Yes, I'd like to recieve the very occasional email with information on new services and discounts</label></p>
+            <input type="submit" value="create an account" className="btn" />
+            <p className="text">Already have a 24Slides account? Click here to log in to your existing account and join a company team</p>
+          </form>
+        )
     )
   }
 }
@@ -66,17 +74,17 @@ class App extends Component {
 const mapState = (state) => {
   return {
     warning: state.warning,
+    signed: state.signed,
   };
 };
 
 const mapDispatch = (dispatch) => {
   return {
     setCountries: (countries) => dispatch({ type: SET_COUNTRIRES, countries: countries }),
-    sendForm: () => dispatch({ type: SEND_FORM, target: event.target }),
+    sendForm: () => dispatch({ type: SEND_FORM, event: event }),
     handleChange: (currentInfo) => dispatch({ type: SET_INFO, currentInfo: currentInfo }),
     validationInput: (currentInput) => dispatch({ type: VAL_INPUT, currentInput: currentInput }),
   };
-
 };
 // const mapDispatch = (dispatch) => {
 //   return {
